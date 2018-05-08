2b84bf529f8d06ac69c5ba30613097f181b18862
Renan Rodrigues <renanrod.dias@gmail.com>
2018-05-08 20:52:49 -0300

    Add files via upload

---
+++ lab-01/temperatura.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+	int temps[5];
+	char temps[201];
+	int media;
+	int soma;
+	int somamedia;
+	for (i=0; i<5; i++) {
+		printf("Digite a temperatura: ");
+		scanf("%d",&temps[i]);
+		soma=soma+temps[i];
+	}
+	media=soma/5;
+	for (i=0; i<5; i++) {
+		if temps[i]>=media {
+			printf("Numeros acima da média: %d\n",temps[i]);
+		}
+	}
+	printf("Média das temperaturas: %d\n",media)		
+	
+
+		
