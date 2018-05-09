c09bce78182e1c1f1bcb92638e7f871913a39e1c
Renan Rodrigues <renanrod.dias@gmail.com>
2018-05-09 19:36:40 +0000

    Update temperatura.c

--- lab-01/temperatura.c
+++ lab-01/temperatura.c
@@ -3,25 +3,29 @@
 int main() {
 
 	int temps[5];
-	char temps[201];
 	int media;
-	int soma;
-	int somamedia;
-	for (i=0; i<5; i++) {
-		printf("Digite a temperatura: ");
+	int soma=0;
+	int i=0;
+	while (i<5) {
+		printf("Digite a %dº temperatura: ", i+1);
 		scanf("%d",&temps[i]);
-		soma=soma+temps[i];
+		if (temps[i]>=-100 && temps[i]<=100) {
+			soma=soma+temps[i];
+			i++;
+		}
+		else {
+			printf("A temperatura deve estar entre -100 e 100\n");
+		}
 	}
 	media=soma/5;
 	for (i=0; i<5; i++) {
-		if temps[i]>=media {
-			printf("Numeros acima da média: %d\n",temps[i]);
+		if (temps[i]>=media) {
+			printf("Temperaturas acima da média: %d\n",temps[i]);
 		}
 	}
-	printf("Média das temperaturas: %d\n",media)
+	printf("Média das temperaturas: %d\n",media);
+	printf("Total das temperaturas: %d\n",soma);
+
 return 0;
 
 }
-	
-
-		
