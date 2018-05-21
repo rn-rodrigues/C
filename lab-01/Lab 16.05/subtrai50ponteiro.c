525e230449075f3fe6872c12d61cb60d6c4314af
Renan Rodrigues <renanrod.dias@gmail.com>
2018-05-21 19:16:54 +0000

    Add files via upload

--- lab-01/Lab 16.05/subtrai50ponteiro.c
+++ lab-01/Lab 16.05/subtrai50ponteiro.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main() {
+	int a,b;
+	int* p;
+	printf("Digite um inteiro a: ");
+	scanf("%d", &a);
+	printf("Digite um inteiro b: ");
+	scanf("%d", &b);
+	printf("Os valores são a=%d / b=%d\n", a, b);
+	if (a>b) {
+		p= &a;
+	}
+	else {
+		p= &b;
+	}
+	*p=*p-50;
+	printf("Os novos valores são: a=%d / b=%d\n",a , b);
+
+return 0;
+
+}
