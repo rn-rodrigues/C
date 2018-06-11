5d2901e01f97b36dd164d426f6ce1294d142a8c4
Renan Rodrigues <renanrod.dias@gmail.com>
2018-06-11 16:33:30 -0300

    Update struct2.c

--- lab-01/Lab 23.05/struct2.c
+++ lab-01/Lab 23.05/struct2.c
@@ -2,29 +2,34 @@
 #include <string.h>
 #define QTDCASA 5
 
-struct casa {
+struct Casa {
 	int nCasa;  // númeração da casa
+	char cor[30]; //Cor da casa
+	  
 };
 
-struct vila {
+struct Vila {
 	char nomeVila[30];
-	struct casa casa[QTDCASA]; //quantidade de casas da Vila
+	struct Casa casa[QTDCASA]; //quantidade de casas da Vila
 };
 
-void preenche2(struct vila* vila) {
+void preenche2(struct Vila* vila) {
 	int i;
 	printf("Nome da Vila: ");
 	scanf("%s", (*vila).nomeVila);
 	for (i=0; i<QTDCASA; i++) {
 		printf("Digite o n° da %d° casa:", i+1);
 		scanf("%d", &((*vila).casa[i].nCasa));
+		printf("Digite a cor da %d° casa: ",i+1);
+		scanf("%s", (*vila).casa[i].cor); 		
 	}
 }
 
 int main() {
-	struct vila vila;
+	struct Vila vila;
 	preenche2(&vila);
 	return 0;
 } 
+
 
 
