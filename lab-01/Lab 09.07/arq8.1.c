f39d02a42ccd4187b7b5802b4a95e0858dd3e229
Renan Rodrigues <renanrod.dias@gmail.com>
2018-07-11 17:38:10 -0300

    Update arq8.1.c

--- lab-01/Lab 09.07/arq8.1.c
+++ lab-01/Lab 09.07/arq8.1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <string.h>
 
+int main() {
+	char cad[25];
+	int i, tamanho;	
+	printf("String:");
+	scanf("%s", cad);
+	tamanho = strlen(cad);
+	FILE* f = fopen("/home/renan/Área de Trabalho/LP1/arq8.1.txt", "w");	
+	for(i=0; i<tamanho; i++) {
+		fputc(cad[i], f);
+	}
+	fclose(f);
+	return 0;
+}
