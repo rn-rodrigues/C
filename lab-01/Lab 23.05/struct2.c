2d290d73be0835e61f0ec8c106311d1c796010d3
Renan Rodrigues <renanrod.dias@gmail.com>
2018-07-11 17:39:55 -0300

    Update struct2.c

--- lab-01/Lab 23.05/struct2.c
+++ lab-01/Lab 23.05/struct2.c
@@ -9,27 +9,30 @@ struct Casa {
 };
 
 struct Vila {
-	char nomeVila[30];
+	char nomeVila[30]; //Nome da Vila
 	struct Casa casa[QTDCASA]; //quantidade de casas da Vila
 };
 
-void preenche2(struct Vila* vila) {
+void preencheCasa(struct Casa* casa) {
+		int i;
+		for (i=0; i<QTDCASA; i++) {
+			printf("Digite o n° da %d° casa:",i+1);
+			scanf("%d", &(*casa).nCasa);
+			printf("Digite a cor da %d° casa: ",i+1);
+			scanf("%s", (*casa).cor); 
+		}
+}
+
+void preencheVila(struct Vila* vila) {
 	int i;
 	printf("Nome da Vila: ");
 	scanf("%s", (*vila).nomeVila);
-	for (i=0; i<QTDCASA; i++) {
-		printf("Digite o n° da %d° casa:", i+1);
-		scanf("%d", &((*vila).casa[i].nCasa));
-		printf("Digite a cor da %d° casa: ",i+1);
-		scanf("%s", (*vila).casa[i].cor); 		
-	}
+   	preencheCasa(&(*vila).casa[i]);
+
 }
 
 int main() {
 	struct Vila vila;
-	preenche2(&vila);
+	preencheVila(&vila);
 	return 0;
-} 
-
-
-
+}
